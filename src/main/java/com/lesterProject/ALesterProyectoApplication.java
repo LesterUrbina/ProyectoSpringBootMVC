72874f69fdddcb5b60cb02c0c3ddb8146b9b8768
lester <a1703583@idat.edu.pe>
2021-11-15 12:42:25 -0500

    primer commit

---
+++ src/main/java/com/lesterProject/ALesterProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.lesterProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALesterProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALesterProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lesterProject/ALesterProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALesterProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
